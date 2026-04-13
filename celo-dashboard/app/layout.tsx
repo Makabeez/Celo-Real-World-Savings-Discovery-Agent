cd709bed25f0f0226bbd27a347516d48925b9653
Makabeez <TON_EMAIL_GITHUB>
2026-04-14 00:43:43 +0200

    chore: add talent.app domain verification tag

--- celo-dashboard/app/layout.tsx
+++ celo-dashboard/app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css'
-import { Providers } from './providers' // <-- Add this import
+import { Providers } from './providers'
 
 export default function RootLayout({
   children,
@@ -8,8 +8,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="talentapp:project_verification" content="9207d630a4593884ac6d64a2bd1ef31fa36434c229196ccd663e4bf353a655fe15511408a65631f6906dc5b32baeb8c895853b7e8a6ee4aada1cc23cfb6b2e47" />
+      </head>
       <body>
-        {/* Wrap children with the Providers */}
         <Providers>
           {children}
         </Providers>
